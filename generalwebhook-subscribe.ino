a208fd671f7347dad4222d9caad5a524465f6320
Andrew Kleindolph <akleindolph@gmail.com>
2019-12-28 15:39:36 -0800

    Update and rename generalwebhook.ino to generalwebhook-subscribe.ino

--- generalwebhook-subscribe.ino
+++ generalwebhook-subscribe.ino
@@ -1,20 +1,12 @@
-// called once on startup
 void setup() {
-    // For simplicity, we'll format our data as text, and pipe it to serial.
-    // but you could just as easily display it in a webpage or pass the data to another system.
-
-    // Learn more about the serial commands at https://docs.particle.io/reference/firmware/photon/#serial
-    //  for the Photon, or https://docs.particle.io/reference/firmware/core/#serial for the Core
-    // You can also watch what's sent over serial with the particle cli with
-    //  particle serial monitor
     Serial.begin(115200);
 
     pinMode(D7, OUTPUT);
 
     // Lets listen for the hook response
-    Particle.subscribe("hook-response/your webhook name", gotData, MY_DEVICES);
+    Particle.subscribe("hook-response/my_webhook", gotData, MY_DEVICES);
 
-    // Lets give ourselves 10 seconds before we actually start the program.
+    // 5 seconds before we actually start the program.
     // That will just give us a chance to open the serial monitor before the program sends the request
     for(int i=0;i<5;i++) {
         Serial.println("waiting " + String(5-i) + " seconds before we publish");
@@ -22,28 +14,21 @@ void setup() {
     }
 }
 
-
-// called forever really fast
 void loop() {
 
     // Let's request the train data, but no more than once every 60 seconds.
     Serial.println("Checking Your Webhook!");
 
     // publish the event that will trigger our Webhook
-    Particle.publish("your webhook name");
+    Particle.publish("my_webhook");
 
     // and wait at least 20 seconds before doing it again
     delay(20000);
 }
 
-// This function will get called when train data comes in
+// This function will get called when data comes in
 void gotData(const char *name, const char *data) {
-    // Important note!  -- Right now the response comes in 512 byte chunks.
-    //  This code assumes we're getting the response in large chunks, and this
-    //  assumption breaks down if a line happens to be split across response chunks.
-    String str = String(data);
-    Serial.println(str);
-    Serial.println();
+    Serial.println(data);
 }
 
 // Returns any text found between a start and end string inside 'str'
